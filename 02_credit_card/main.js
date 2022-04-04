410cb22da07b452dddf3d7a6fb05b5988f019e6f
Vera Mazhuga <vmazhuga@hugeinc.com>
2022-04-04 10:36:23 -0500

    Added a form for credit card

---
+++ 02_credit_card/main.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app",
+    data: {
+        number: 'XXXX XXXX XXXX XXXX',
+        displayed_name: 'XXXXXXX XXXXXXXXXX',
+        expiration_month: 'XX',
+        expiration_year: 'XX',
+        ccv: '',
+    },
+})
