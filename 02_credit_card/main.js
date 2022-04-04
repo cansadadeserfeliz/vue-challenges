8a8e259328c53484497fe1c8b8651f84282b8f2c
Vera Mazhuga <vmazhuga@hugeinc.com>
2022-04-04 13:00:01 -0500

    Connect form fields with vue model and use compute for display values

--- 02_credit_card/main.js
+++ 02_credit_card/main.js
@@ -1,10 +1,36 @@
 new Vue({
     el: "#app",
     data: {
-        number: 'XXXX XXXX XXXX XXXX',
-        displayed_name: 'XXXXXXX XXXXXXXXXX',
-        expiration_month: 'XX',
-        expiration_year: 'XX',
+        number: '',
+        card_holder_name: '',
+        expiration_month: '01',
+        expiration_year: null,
         ccv: '',
     },
+    computed: {
+        numberDisplay: function () {
+            let number = 'XXXXXXXXXXXXXXXX';
+            if (this.number) {
+                number = this.number;
+            }
+            // Split into segments of 4 characters
+            return number.match(/.{1,4}/g).join(" ");
+        },
+        cardHolderNameDisplay: function () {
+            let card_holder_name = 'XXXXXXX XXXXXXXXXX';
+            if (this.card_holder_name) {
+                card_holder_name = this.card_holder_name;
+            }
+            return card_holder_name;
+        },
+        expirationMonthDisplay : function () {
+            return this.expiration_month;
+        },
+        expirationYearDisplay : function () {
+            return this.expiration_year || 'XX';
+        },
+        ccvDisplay : function () {
+            return this.ccv || 'XXX';
+        }
+    }
 })
